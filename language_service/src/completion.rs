e4cac2f8307ea78e8f0b9cef6c0e4569caa1bbaf
Mine Starks <mineyalc@microsoft.com>
2023-06-24 16:24:49 -0700

    Filter duplicates, remove debug items

--- language_service/src/completion.rs
+++ language_service/src/completion.rs
@@ -41,6 +41,7 @@ pub struct CompletionItem {
     pub kind: CompletionItemKind,
 }
 
+#[allow(clippy::too_many_lines)]
 pub(crate) fn get_completions(
     compilation: &Compilation,
     source_name: &str,
@@ -57,129 +58,138 @@ pub(crate) fn get_completions(
 
     let mut items = Vec::new();
 
+    // Just making sure certain things only get added once
+    // whats_next is still a bit fuzzy and returns the same
+    // constraint multiple times sometimes
+    let mut names_added = None;
+    let mut types_added = None;
+    let mut namespaces_added = None;
+    let mut keywords_added = HashSet::new();
+
     for completion_constraint in qsc_utils::whats_next(truncated_source) {
-        add_completions(completion_constraint, compilation, offset, &mut items);
+        match completion_constraint {
+            qsc::CompletionConstraint::Path => {
+                names_added.get_or_insert_with(|| {
+                    let (names, namespaces) = gather_names(
+                        &compilation.package_store,
+                        &[compilation.std_package_id],
+                        &compilation.ast_package,
+                        offset,
+                        &GatherOptions::NamespacesAndTerms,
+                    );
+                    for name in names {
+                        items.push(CompletionItem {
+                            label: name.to_string(),
+                            kind: CompletionItemKind::Function,
+                        });
+                    }
+                    namespaces_added.get_or_insert_with(|| {
+                        for namespace in namespaces {
+                            items.push(CompletionItem {
+                                label: namespace.to_string(),
+                                kind: CompletionItemKind::Module,
+                            });
+                        }
+                    });
+                });
+            }
+            qsc::CompletionConstraint::Ty => {
+                types_added.get_or_insert_with(|| {
+                    let (names, namespaces) = gather_names(
+                        &compilation.package_store,
+                        &[compilation.std_package_id],
+                        &compilation.ast_package,
+                        offset,
+                        &GatherOptions::NamespacesAndTypes,
+                    );
+                    for name in names {
+                        items.push(CompletionItem {
+                            label: name.to_string(),
+                            kind: CompletionItemKind::Interface,
+                        });
+                    }
+                    namespaces_added.get_or_insert_with(|| {
+                        for namespace in namespaces {
+                            items.push(CompletionItem {
+                                label: namespace.to_string(),
+                                kind: CompletionItemKind::Module,
+                            });
+                        }
+                    });
+                });
+            }
+            qsc::CompletionConstraint::Namespace => {
+                let (_, namespaces) = gather_names(
+                    &compilation.package_store,
+                    &[compilation.std_package_id],
+                    &compilation.ast_package,
+                    offset,
+                    &GatherOptions::NamespacesAndTypes,
+                );
+                namespaces_added.get_or_insert_with(|| {
+                    for namespace in namespaces {
+                        items.push(CompletionItem {
+                            label: namespace.to_string(),
+                            kind: CompletionItemKind::Module,
+                        });
+                    }
+                });
+            }
+            qsc::CompletionConstraint::Qubit => {
+                items.push(CompletionItem {
+                    label: "Qubit".to_string(),
+                    kind: CompletionItemKind::Interface,
+                });
+            }
+            qsc::CompletionConstraint::Keyword(keyword) => {
+                if keywords_added.insert(keyword.clone()) {
+                    items.push(CompletionItem {
+                        label: keyword,
+                        kind: CompletionItemKind::Keyword,
+                    });
+                }
+            }
+            qsc::CompletionConstraint::Field => {
+                items.push(CompletionItem {
+                    label: "[field options]".to_string(),
+                    kind: CompletionItemKind::Issue,
+                });
+            }
+            qsc::CompletionConstraint::Attr => {
+                items.push(CompletionItem {
+                    label: "[attr options]".to_string(),
+                    kind: CompletionItemKind::Issue,
+                });
+            }
+            qsc::CompletionConstraint::TyParam => {
+                items.push(CompletionItem {
+                    label: "[typaram options]".to_string(),
+                    kind: CompletionItemKind::Issue,
+                });
+            }
+            _ => {} // qsc::CompletionConstraint::Binding => {
+                    //     items.push(CompletionItem {
+                    //         label: "~BINDING".to_string(),
+                    //         kind: CompletionItemKind::Issue,
+                    //     });
+                    // }
+                    // qsc::CompletionConstraint::Other(t) => {
+                    //     items.push(CompletionItem {
+                    //         label: format!("~TOKEN {t}"),
+                    //         kind: CompletionItemKind::Issue,
+                    //     });
+                    // }
+                    // qsc::CompletionConstraint::Debug(s) => {
+                    //     items.push(CompletionItem {
+                    //         label: format!("~~DEBUG {s}"),
+                    //         kind: CompletionItemKind::Issue,
+                    //     });
+                    // }
+        }
     }
 
     CompletionList { items }
 }
-
-#[allow(clippy::too_many_lines)]
-fn add_completions(
-    constraint: qsc::CompletionConstraint,
-    compilation: &Compilation,
-    offset: u32,
-    items: &mut Vec<CompletionItem>,
-) {
-    match constraint {
-        qsc::CompletionConstraint::Path => {
-            let (names, namespaces) = gather_names(
-                &compilation.package_store,
-                &[compilation.std_package_id],
-                &compilation.ast_package,
-                offset,
-                &GatherOptions::NamespacesAndTerms,
-            );
-            for name in names {
-                items.push(CompletionItem {
-                    label: name.to_string(),
-                    kind: CompletionItemKind::Function,
-                });
-            }
-            for namespace in namespaces {
-                items.push(CompletionItem {
-                    label: namespace.to_string(),
-                    kind: CompletionItemKind::Module,
-                });
-            }
-        }
-        qsc::CompletionConstraint::Ty => {
-            let (names, namespaces) = gather_names(
-                &compilation.package_store,
-                &[compilation.std_package_id],
-                &compilation.ast_package,
-                offset,
-                &GatherOptions::NamespacesAndTypes,
-            );
-            for name in names {
-                items.push(CompletionItem {
-                    label: name.to_string(),
-                    kind: CompletionItemKind::Interface,
-                });
-            }
-            for namespace in namespaces {
-                items.push(CompletionItem {
-                    label: namespace.to_string(),
-                    kind: CompletionItemKind::Module,
-                });
-            }
-        }
-        qsc::CompletionConstraint::Namespace => {
-            let (_, namespaces) = gather_names(
-                &compilation.package_store,
-                &[compilation.std_package_id],
-                &compilation.ast_package,
-                offset,
-                &GatherOptions::NamespacesAndTypes,
-            );
-            for namespace in namespaces {
-                items.push(CompletionItem {
-                    label: namespace.to_string(),
-                    kind: CompletionItemKind::Module,
-                });
-            }
-        }
-        qsc::CompletionConstraint::Qubit => {
-            items.push(CompletionItem {
-                label: "Qubit".to_string(),
-                kind: CompletionItemKind::Interface,
-            });
-        }
-        qsc::CompletionConstraint::Keyword(keyword) => {
-            items.push(CompletionItem {
-                label: keyword,
-                kind: CompletionItemKind::Keyword,
-            });
-        }
-        qsc::CompletionConstraint::Field => {
-            items.push(CompletionItem {
-                label: "[field options]".to_string(),
-                kind: CompletionItemKind::Issue,
-            });
-        }
-        qsc::CompletionConstraint::Attr => {
-            items.push(CompletionItem {
-                label: "[attr options]".to_string(),
-                kind: CompletionItemKind::Issue,
-            });
-        }
-        qsc::CompletionConstraint::TyParam => {
-            items.push(CompletionItem {
-                label: "[typaram options]".to_string(),
-                kind: CompletionItemKind::Issue,
-            });
-        }
-        qsc::CompletionConstraint::Binding => {
-            items.push(CompletionItem {
-                label: "~BINDING".to_string(),
-                kind: CompletionItemKind::Issue,
-            });
-        }
-        qsc::CompletionConstraint::Debug(s) => {
-            items.push(CompletionItem {
-                label: format!("~~DEBUG {s}"),
-                kind: CompletionItemKind::Issue,
-            });
-        }
-        qsc::CompletionConstraint::Other(t) => {
-            items.push(CompletionItem {
-                label: format!("~TOKEN {t}"),
-                kind: CompletionItemKind::Issue,
-            });
-        }
-    }
-}
-
 struct NamespaceCollector {
     namespaces: HashSet<String>,
 }
